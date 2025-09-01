a9237e585d5f4eb569e03454aa536c08349ff1d7
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-09-01 16:02:34 +0700

    Day 1.1 belajar java untuk mengasah ke skripsi

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Hello, Broo!");
+        System.out.println("baris ke-2: hai juga bro");
+
+        System.out.print("baris ke-3: ini baris ketiga");
+        System.out.print("baris ke-4: saya meneruskan baris ketiga");
+
+        System.out.print("baris ke-5: test baris ke 5");
+        System.out.println("baris ke-6: baris ke 6");
+
+        System.out.print("baris ke-7: baris ke 7 \n");
+        System.out.print("baris ke-8: ini baris kedelapan \n");
+        System.out.print("baris ke-9: saya meneruskan baris kesembilan \n");
+
+        System.out.printf("baris ke-10: halo ketua %d", 212);
     }
 }
