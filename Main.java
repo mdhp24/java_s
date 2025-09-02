1833bfb0677c48e64c7378f8d7e8677ddfa2cd1c
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-09-02 15:46:05 +0700

    Day 2 belajar java untuk mengasah ke skripsi

--- Main.java
+++ Main.java
@@ -1,19 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, Broo!");
-        System.out.println("baris ke-2: hai juga bro");
+        // System.out.println("Hello, Broo!");
+        // System.out.println("baris ke-2: hai juga bro");
 
-        System.out.print("baris ke-3: ini baris ketiga");
-        System.out.print("baris ke-4: saya meneruskan baris ketiga");
+        // System.out.print("baris ke-3: ini baris ketiga");
+        // System.out.print("baris ke-4: saya meneruskan baris ketiga");
 
-        System.out.print("baris ke-5: test baris ke 5");
-        System.out.println("baris ke-6: baris ke 6");
+        // System.out.print("baris ke-5: test baris ke 5");
+        // System.out.println("baris ke-6: baris ke 6");
 
-        System.out.print("baris ke-7: baris ke 7 \n");
-        System.out.print("baris ke-8: ini baris kedelapan \n");
-        System.out.print("baris ke-9: saya meneruskan baris kesembilan \n");
+        // System.out.print("baris ke-7: baris ke 7 \n");
+        // System.out.print("baris ke-8: ini baris kedelapan \n");
+        // System.out.print("baris ke-9: saya meneruskan baris kesembilan \n");
 
-        System.out.printf("baris ke-10: halo ketua %d", 212);
+        // System.out.printf("baris ke-10: halo ketua %d", 212);
+
+        System.out.println("hallo cuy" + args[0] + "posisi?");
+        System.out.println(args[1]);
+
+        // System.out.println("halllooo");
     }
 }
