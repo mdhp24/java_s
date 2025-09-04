9c563eeff4415377ec3a42abd98659a97fb2883f
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-09-04 20:55:01 +0700

    Day 4.6 belajar java untuk mengasah ke skripsi

--- Main.java
+++ Main.java
@@ -422,22 +422,22 @@ public class Main {
         // } while (i <= nilaiAkhir);
 
         // menghitung nilai deret fibonacci ke-n
-        int f_n, f_n_1, f_n_2, n;
+        // int f_n, f_n_1, f_n_2, n;
 
-        Scanner inputUser = new Scanner(System.in);
-        System.out.print("mengambil nilai fibonacci ke - :");
-        n = inputUser.nextInt();
+        // Scanner inputUser = new Scanner(System.in);
+        // System.out.print("mengambil nilai fibonacci ke - :");
+        // n = inputUser.nextInt();
 
-        f_n_2 = 0;
-        f_n_1 = 1;
-        f_n = 1;
+        // f_n_2 = 0;
+        // f_n_1 = 1;
+        // f_n = 1;
 
-        for (int i = 1; i <= n; i++) {
-            System.out.println("nilai ke - " + i + " adalah " + f_n);
-            f_n = f_n_1 + f_n_2;
-            f_n_2 = f_n_1;
-            f_n_1 = f_n;
-        }
+        // for (int i = 1; i <= n; i++) {
+        //     System.out.println("nilai ke - " + i + " adalah " + f_n);
+        //     f_n = f_n_1 + f_n_2;
+        //     f_n_2 = f_n_1;
+        //     f_n_1 = f_n;
+        // }
 
     }
 
