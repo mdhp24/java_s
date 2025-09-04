b96492cec05e8e8a54efc40f82827dface250bc4
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-09-04 19:36:07 +0700

    Day 4.1 belajar java untuk mengasah ke skripsi

--- Main.java
+++ Main.java
@@ -294,38 +294,80 @@ public class Main {
         // System.out.println("awal program");
 
         // while (kondisi) {
-        //     System.out.println("while loop ke-" + a);
+        // System.out.println("while loop ke-" + a);
 
-        //     if (a == 20) {
-        //         kondisi = false;
-        //     }
-            
-        //     a++;
+        // if (a == 20) {
+        // kondisi = false;
+        // }
+
+        // a++;
 
         // }
 
         // System.out.println("akhir program");
 
-        //        do {
-//            aksi
-//        } while (kondisi);
+        // do {
+        // aksi
+        // } while (kondisi);
 
+        // System.out.println("ini adalah awal program");
+
+        // int a = 0;
+        // boolean kondisi = true;
+
+        // do {
+        // a++;
+        // System.out.println("do while ke-" + a);
+
+        // if (a == 1) {
+        // kondisi = false;
+        // }
+
+        // }while(kondisi);
+
+        // System.out.println("ini adalah akhir program");
+
+        // for (inisialisasi; kondisi; step nilai){
+        //
+        // aksi;
+        //
+        // }
 
         System.out.println("ini adalah awal program");
 
-        int a = 0;
-        boolean kondisi = true;
+        System.out.println("loop pertama");
 
+        for (int nilai = 0; nilai <= 10; nilai++) {
 
-        do {
-            a++;
-            System.out.println("do while ke-" + a);
+            System.out.println("for loop ke-" + nilai);
 
-            if (a == 1) {
-                kondisi = false;
-            }
+        }
 
-        }while(kondisi);
+        System.out.println("loop kedua");
+
+        for (int nilai = 0; nilai < 10; nilai++) {
+
+            System.out.println("for loop ke-" + nilai);
+
+        }
+
+        System.out.println("loop ketiga");
+
+        for (int nilai = 10; nilai >= 5; nilai--) {
+
+            System.out.println("for loop ke-" + nilai);
+
+        }
+
+        System.out.println("loop keempat");
+
+        int nilai = 0;
+
+        for (; nilai < 10;) {
+
+            System.out.println("for loop ke-" + nilai);
+            nilai++;
+        }
 
         System.out.println("ini adalah akhir program");
 
