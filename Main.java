dcb6bda3c28b224a60fc745bcb8d8e22ca05049e
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-09-05 23:25:23 +0700

    Day 5.7 belajar java untuk mengasah ke skripsi

--- Main.java
+++ Main.java
@@ -666,13 +666,23 @@ public class Main {
         // }
 
         // Program dari PSEUDOCODE mencari luas lapangan sepak bola
-        int panjang, lebar, luas;
+        // int panjang, lebar, luas;
 
-        panjang = 100;
-        lebar = 64;
-        luas = panjang * lebar;
+        // panjang = 100;
+        // lebar = 64;
+        // luas = panjang * lebar;
 
-        System.out.println("Luas lapangan sepak bola adalah " + luas);
+        // System.out.println("Luas lapangan sepak bola adalah " + luas);
+
+        // Program dari PSEUDOCODE menentukan sebuah bilangan adalah ganjil atau genap
+        int bilangan;
+
+        bilangan = 99;
+        if (bilangan % 2 == 0) {
+            System.out.print(bilangan + " Bilangan genap");
+        } else {
+            System.out.print(bilangan + " Bilangan ganjil");
+        }
 
     }
 }
