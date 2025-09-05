1978a2784bcbeb4d3a3c6dc8f7a1029dc9f08152
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-09-05 23:02:27 +0700

    Day 5.2 belajar java untuk mengasah ke skripsi

--- Main.java
+++ Main.java
@@ -559,6 +559,21 @@ public class Main {
             System.out.print("\n");
         }
 
+        System.out.print("\n");
+
+        for (int i = 0; i < 9; i++) {
+            // System.out.println("loop ke-" + i);
+            for (int j = 0; j < 9; j++) {
+                System.out.print("* ");
+                if (i == j) {
+                    break;
+                } else if ((i + j == 8)){
+                    break;
+                }
+            }
+            System.out.print("\n");
+        }
+
     }
 
 }
