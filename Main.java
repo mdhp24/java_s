caaaa6666b1736b28609b54155b9d2035a7a5ecb
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-09-06 13:39:44 +0700

    Day 6 belajar java untuk mengasah ke skripsi

--- Main.java
+++ Main.java
@@ -685,12 +685,58 @@ public class Main {
         // }
 
         // Program dari PSEUDOCODE menghitung mundur
-        int bilangan;
+        // int bilangan;
 
-        for (bilangan = 9; bilangan > 0; bilangan--) {
-            System.out.println("Bilangan adalah " + bilangan);
-        }
+        // for (bilangan = 9; bilangan > 0; bilangan--) {
+        // System.out.println("Bilangan adalah " + bilangan);
+        // }
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("masukan nilai:");
+        int nilai = userInput.nextInt();
+        System.out.println("anda memasukan nilai = " + nilai);
+
+        // printNilai(nilai);
+        // int jumlah = jumlahNilai(nilai);
+        // System.out.println("jumlah = " + jumlah);
+
+        int faktorial = hitungFaktorial(nilai);
+        System.out.println("hasil faktorial = " + faktorial);
     }
+    // fungsi rekursif sederhana
+
+    private static int hitungFaktorial(int parameter) {
+        System.out.println("parameter = " + parameter);
+
+        if (parameter == 1) {
+            return parameter;
+        }
+
+        return parameter * hitungFaktorial(parameter - 1);
+
+    }
+
+    // private static int jumlahNilai(int parameter) {
+    //     System.out.println("parameter = " + parameter);
+
+    //     if (parameter == 0) {
+    //         return parameter;
+    //     }
+
+    //     return parameter + jumlahNilai(parameter - 1);
+
+    // }
+
+    // private static void printNilai(int parameter) {
+    //     System.out.println("nilai = " + parameter);
+
+    //     if (parameter == 0) {
+    //         return;
+    //     }
+
+    //     parameter--;
+
+    //     printNilai(parameter);
+    // }
 }
 // }
 
