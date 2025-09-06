089bf8aa036fe8714e5f3b2da6eec04532c346ae
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-09-06 13:47:44 +0700

    Day 6.3 belajar java untuk mengasah ke skripsi

--- Main.java
+++ Main.java
@@ -690,52 +690,69 @@ public class Main {
         // for (bilangan = 9; bilangan > 0; bilangan--) {
         // System.out.println("Bilangan adalah " + bilangan);
         // }
+        // Scanner userInput = new Scanner(System.in);
+        // System.out.print("masukan nilai:");
+        // int nilai = userInput.nextInt();
+        // System.out.println("anda memasukan nilai = " + nilai);
+
+        // printNilai(nilai);
+        // int jumlah = jumlahNilai(nilai);
+        // System.out.println("jumlah = " + jumlah);
+
+        // int faktorial = hitungFaktorial(nilai);
+        // System.out.println("hasil faktorial = " + faktorial);
+        // }
+        // fungsi rekursif sederhana
+
+        // private static int hitungFaktorial(int parameter) {
+        // System.out.println("parameter = " + parameter);
+
+        // if (parameter == 1) {
+        // return parameter;
+        // }
+
+        // return parameter * hitungFaktorial(parameter - 1);
+
+        // }
+
+        // private static int jumlahNilai(int parameter) {
+        // System.out.println("parameter = " + parameter);
+
+        // if (parameter == 0) {
+        // return parameter;
+        // }
+
+        // return parameter + jumlahNilai(parameter - 1);
+
+        // }
+
+        // private static void printNilai(int parameter) {
+        // System.out.println("nilai = " + parameter);
+
+        // if (parameter == 0) {
+        // return;
+        // }
+
+        // parameter--;
+
+        // printNilai(parameter);
+        // }
+
         Scanner userInput = new Scanner(System.in);
-        System.out.print("masukan nilai:");
+        System.out.print("masukan nilai n = ");
         int nilai = userInput.nextInt();
-        System.out.println("anda memasukan nilai = " + nilai);
-
-        printNilai(nilai);
-        int jumlah = jumlahNilai(nilai);
-        System.out.println("jumlah = " + jumlah);
-
-        int faktorial = hitungFaktorial(nilai);
-        System.out.println("hasil faktorial = " + faktorial);
-    }
-    // fungsi rekursif sederhana
-
-    private static int hitungFaktorial(int parameter) {
-        System.out.println("parameter = " + parameter);
-
-        if (parameter == 1) {
-            return parameter;
-        }
-
-        return parameter * hitungFaktorial(parameter - 1);
-
+        int nilai_fibonacci = fibonacci(nilai, "atas");
+        System.out.println("nilai fibonacci ke-" + nilai + " adalah " + nilai_fibonacci);
     }
 
-    private static int jumlahNilai(int parameter) {
-        System.out.println("parameter = " + parameter);
-
-        if (parameter == 0) {
-            return parameter;
+    private static int fibonacci(int n, String daun) {
+        System.out.println("daun " + daun);
+        System.out.println("Fibonacci ke - " + n);
+        if (n == 1 || n == 0) {
+            return n;
+        } else {
+            return fibonacci(n - 1, "kiri") + fibonacci(n - 2, "kanan");
         }
-
-        return parameter + jumlahNilai(parameter - 1);
-
-    }
-
-    private static void printNilai(int parameter) {
-        System.out.println("nilai = " + parameter);
-
-        if (parameter == 0) {
-            return;
-        }
-
-        parameter--;
-
-        printNilai(parameter);
     }
 }
 // }
